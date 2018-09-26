4c6f4aa143a547e92ec62d426baf3ca2fad43f39
Tom Hsu <thomaschsu@gmail.com>
2018-09-26 00:33:33 -0400

    Fixed navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
       MyMovieList
     </a>
