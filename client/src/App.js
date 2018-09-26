3dbc42a327f9c083b4d0e3fa6f1bb7430dfb1f4d
Tom Hsu <thomaschsu@gmail.com>
2018-09-26 15:36:26 -0400

    Fixed some styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
 
 const App = () => (
   <Router>
     <div>
       <Nav />
-      <Switch>
+      <Wrapper>
+        <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
+      </Wrapper>
+      
     </div>
   </Router>
 );
