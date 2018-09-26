aedcf77b434e914b381192241ddd4a8d78751e88
Tom Hsu <thomaschsu@gmail.com>
2018-09-26 00:24:57 -0400

    Added nav css and font

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
-      React Reading List
+      MyMovieList
     </a>
   </nav>
 );
