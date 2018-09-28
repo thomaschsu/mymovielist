7d013a9760c464a07d710381fb96d7accc73fd03
Tom Hsu <thomaschsu@gmail.com>
2018-09-27 23:51:08 -0400

    Finished fixing react tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,140 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import MovieNav from "./components/MovieNav";
 import Wrapper from "./components/Wrapper";
-import Jumbotron from "./components/Jumbotron";
-import List from "./components/List";
+import MovieList from "./pages/MovieList";
+import Home from "./pages/Home";
 
-class App extends Component {
-	state = {
-		movies: [
-			{
-				image: "sample image",
-				status: "completed",
-				title: "sample title",
-				score: "10",
-				director: "sample director",
-			},
-			{
-				image: "different image",
-				status: "dropped",
-				title: "different title",
-				score: "4",
-				director: "different director",
-			},
-			{
-				image: "one more image",
-				status: "ptw",
-				title: "one more title",
-				score: "-",
-				director: "one more director",
-			}
-		]
-	};
+const App = () => (
 
-	getMovies = cb => {
-		this.setState({
-			movies: [
-				{
-					image: "sample image",
-					status: "completed",
-					title: "sample title",
-					score: "10",
-					director: "sample director",
-				},
-				{
-					image: "different image",
-					status: "dropped",
-					title: "different title",
-					score: "4",
-					director: "different director",
-				},
-				{
-					image: "one more image",
-					status: "ptw",
-					title: "one more title",
-					score: "-",
-					director: "one more director",
-				}
-			]
-		}, cb);
-	};
-
-	handleStatusChange = event => {
-		switch (event.target.textContent) {
-			//All Movies List
-			case "All Movies":
-				console.log("all route");
-				this.getMovies(() => {});
-				break;
-
-			//Completed List
-			case "Completed":
-				console.log("completed route");
-				this.getMovies(() => {
-					const completedMovies = this.state.movies.filter(movie => (
-						movie.status === "completed"
-					));
-
-					this.setState({
-						movies: completedMovies
-					});
-				});
-
-				break;
-
-			//Dropped List
-			case "Dropped":
-				console.log("dropped route");
-				this.getMovies(() => {
-					const droppedMovies = this.state.movies.filter(movie => (
-						movie.status === "dropped"
-					));
-
-					this.setState({
-						movies: droppedMovies
-					});
-				});
-
-				break;
-
-			//PTW List
-			case "Plan to Watch":
-				console.log("ptw route");
-				this.getMovies(() => {
-					const ptwMovies = this.state.movies.filter(movie => (
-						movie.status === "ptw"
-					));
-
-					this.setState({
-						movies: ptwMovies
-					});
-				});
-
-				break;
-		}
-	};
-
-	componentDidMount = () => {
-		this.getMovies(() => {});
-	};
-
-	render() {
-		return (
-			<div>
-				<Nav />
-				<Wrapper>
-					<Jumbotron>
-						<h1 className="jumbo-small">Organize, Discuss, Discover</h1>
-						<h1 className="jumbo-title">MyMovieList</h1>
-						<h1 className="jumbo-small">HOW MANY MOVIES HAVE YOU SEEN?</h1>
-					</Jumbotron>
-					<MovieNav function={this.handleStatusChange}></MovieNav>
-					<List movies={this.state.movies} />
-				</Wrapper>
-			</div>
-		);
-	};
-};
+	<Router>
+		<div>
+			<Nav />
+			<Wrapper>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/movielist" component={MovieList} />
+			</Wrapper>
+		</div>
+	</Router>
+);
 
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => (
   <nav className="bg-primary nav">
-    <a className="navbar-brand" href="/">
+    <Link className="navbar-brand" to="/">
       MyMovieList
-    </a>
+    </Link>
+    <div>
+      <ul className="navbar-nav right">
+        <li
+          className={
+            window.location.pathname === "/"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/movielist"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/movielist" className="nav-link">
+            Movie List
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/search"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/search" className="nav-link">
+            Search
+          </Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
