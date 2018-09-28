f727fd84a7fe5dbeb04c520d179968c0ec55343a
Tom Hsu <thomaschsu@gmail.com>
2018-09-28 16:44:48 -0400

    Finished movie nav css

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -42,17 +42,8 @@ const Nav = () => (
             Search
           </Link>
         </li>
-        <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Login</a>
-        <a class="waves-effect waves-light btn modal-trigger" href="#modal2">Sign Up</a>
-        <div id="modal1" class="modal">
-          <div class="modal-content">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
-          </div>
-          <div class="modal-footer">
-            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-          </div>
-        </div>
+        <a className="waves-effect waves-light btn blue">Login</a>
+        <a className="waves-effect waves-light btn blue">Sign Up</a>
       </ul>
     </div>
   </nav>
