a9b2e04c940cb42a18c097b6a4264d1884800ed5
thomaschsu <thomaschsu@gmail.com>
2018-10-06 11:52:55 -0400

    Added dropdown

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -5,8 +5,25 @@ const ListItem = props => (
     <td></td>
     <td><img src={props.image === "N/A" || !props.image ? "/nopicture.png" : props.image} alt="displays site name"></img></td>
     <td>{props.title}</td>
-    <td>{props.score}</td>
-    <td>{props.status}</td>
+    <td>
+      <select>
+        <option value="10">10</option>
+        <option value="9">9</option>
+        <option value="8">8</option>
+        <option value="7">7</option>
+        <option value="6">6</option>
+        <option value="5">5</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+        <option value="2">2</option>
+        <option value="1">1</option>
+
+      </select></td>
+    <td><select>
+        <option value="completed">Completed</option>
+        <option value="dropped">Dropped</option>
+        <option value="plantowatch">Plan to Watch</option>
+      </select></td>
     <td>{props.director}</td>
   </tr>
 );
