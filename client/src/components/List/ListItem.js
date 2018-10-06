5d7ede2f4ebd389cbaa67e7bc6b5d65f22791311
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-06 12:01:34 -0400

    made dropdowns show current value by default

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -7,22 +7,22 @@ const ListItem = props => (
     <td>{props.title}</td>
     <td>
       <select>
-        <option value="10">10</option>
-        <option value="9">9</option>
-        <option value="8">8</option>
-        <option value="7">7</option>
-        <option value="6">6</option>
-        <option value="5">5</option>
-        <option value="4">4</option>
-        <option value="3">3</option>
-        <option value="2">2</option>
-        <option value="1">1</option>
-
+        <option value="10" selected={props.score === "10"}>10</option>
+        <option value="9" selected={props.score === "9"}>9</option>
+        <option value="8" selected={props.score === "8"}>8</option>
+        <option value="7" selected={props.score === "7"}>7</option>
+        <option value="6" selected={props.score === "6"}>6</option>
+        <option value="5" selected={props.score === "5"}>5</option>
+        <option value="4" selected={props.score === "4"}>4</option>
+        <option value="3" selected={props.score === "3"}>3</option>
+        <option value="2" selected={props.score === "2"}>2</option>
+        <option value="1" selected={props.score === "1"}>1</option>
+        <option value="-" selected={props.score === "-"}>-</option>
       </select></td>
     <td><select>
-        <option value="completed">Completed</option>
-        <option value="dropped">Dropped</option>
-        <option value="plantowatch">Plan to Watch</option>
+        <option value="completed" selected={props.status === "completed"}>Completed</option>
+        <option value="dropped" selected={props.status === "dropped"}>Dropped</option>
+        <option value="ptw" selected={props.status === "ptw"}>Plan to Watch</option>
       </select></td>
     <td>{props.director}</td>
   </tr>
