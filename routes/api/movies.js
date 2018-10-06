eb546906b4c502ee849661a9d8be02ad069470d9
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-06 11:13:33 -0400

    setup route for adding movies

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const moviesController = require("../../controllers/moviesController");
 
 router.route("/:user")
-  .get(moviesController.findAll);
+  .get(moviesController.findAll)
+  .post(moviesController.addMovie);
 
 module.exports = router;
