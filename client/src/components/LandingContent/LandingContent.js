8fe3120b0000455abbbfbef75804631236e6e0b3
thomaschsu <thomaschsu@gmail.com>
2018-10-06 10:46:27 -0400

    Modifed class to classname

--- client/src/components/LandingContent/LandingContent.js
+++ client/src/components/LandingContent/LandingContent.js
@@ -5,7 +5,7 @@ const LandingContent = () => (
     <div className="landingcontent"><table>
         <tbody>
             <tr>
-                <td className="table-format-left animated bounceInDown delay-2s"><h5 className="table-heading red-text"><i class="fas fa-user-friends"></i> Share with your friends.</h5><br></br><span>With a thriving community, chat with others and compare lists with friends!</span></td>
+                <td className="table-format-left animated bounceInDown delay-2s"><h5 className="table-heading red-text"><i className="fas fa-user-friends"></i> Share with your friends.</h5><br></br><span>With a thriving community, chat with others and compare lists with friends!</span></td>
                 <td><div className="row">
                     <div className="col s12 m7">
                         <div className="card animated fadeInUp delay-2s">
@@ -21,10 +21,10 @@ const LandingContent = () => (
             </tr>
             <tr>
                 <td className="table-format-left fadeInLeftBig animated delay"><img src="poster.jpg" className="img-poster"></img></td>
-                <td className="table-right bounceInRight animated delay"><h5 className="table-heading yellow-text"><i class="fas fa-star-half"></i> Score Your Movies</h5><br></br>Score the movies on your list from 0-10. Did you like the movie? Score a 10. Hate the movie? Score a 0.</td>
+                <td className="table-right bounceInRight animated delay"><h5 className="table-heading yellow-text"><i className="fas fa-star-half"></i> Score Your Movies</h5><br></br>Score the movies on your list from 0-10. Did you like the movie? Score a 10. Hate the movie? Score a 0.</td>
             </tr>
             <tr>
-                <td className="table-format-left bounceInLeft animated delay-2"><h5 className="table-heading green-text"><i class="far fa-lightbulb"></i> Never Forget</h5><br></br>Never forget the movies you watched or what scores you gave. Keep track of it all in one place.</td>
+                <td className="table-format-left bounceInLeft animated delay-2"><h5 className="table-heading green-text"><i className="far fa-lightbulb"></i> Never Forget</h5><br></br>Never forget the movies you watched or what scores you gave. Keep track of it all in one place.</td>
                 <td><div className="row">
                     <div className="col s12 m5">
                         <div className="card-panel deep-purple flipInY animated delay-2">
