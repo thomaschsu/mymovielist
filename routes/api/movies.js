34dc340c82d474d81fc610722e7465f4b0a3cbc9
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-06 12:54:15 -0400

    added route for removing movie

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -4,6 +4,7 @@ const moviesController = require("../../controllers/moviesController");
 router.route("/:user")
   .get(moviesController.findAll)
   .post(moviesController.addMovie)
-  .put(moviesController.updateMovie);
+  .put(moviesController.updateMovie)
+  .delete(moviesController.removeMovie);
 
 module.exports = router;
