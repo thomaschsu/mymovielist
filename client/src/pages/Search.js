6b37d32d3f0743f6530ffb4b194621743b791874
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-06 11:13:55 -0400

    adding to list working

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -27,7 +27,18 @@ class Search extends Component {
 	};
 
 	handleAddToList = event => {
-
+		const movieId = event.target.dataset.imdb;
+		let movie = {};
+		API.searchOne(movieId).then(res => {
+			movie = {
+				title: res.data.Title,
+				score: "-",
+				status: "completed",
+				image: res.data.Poster,
+				director: res.data.Director
+			};
+			API.addMovie(sessionStorage.getItem("username").slice(1, -1), movie)
+		});
 	};
 
 	render() {
@@ -36,7 +47,7 @@ class Search extends Component {
 				<Wrapper>
 					<SearchBar changefunc={this.handleInputChange} submitfunc={this.handleFormSubmit} />
 					<div>
-						{this.state.results.map(element => <SearchResult title={element.Title} image={element.Poster} year={element.Year} key={element.imdbID} id={element.imdbID} />)}
+						{this.state.results.map(element => <SearchResult title={element.Title} image={element.Poster} year={element.Year} key={element.imdbID} imdb={element.imdbID} click={this.handleAddToList} />)}
 					</div>
 				</Wrapper>
 			</div>
