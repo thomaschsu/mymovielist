2ff4ad9b7757ce4334104916c38e34c072177593
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-06 12:35:21 -0400

    dropdowns save new value to database

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -14,5 +14,31 @@ module.exports = {
 				{ $addToSet: { movieArr: req.body } }
 			).then(dbModel => res.json(dbModel))
 			.catch(err => res.status(422).json(err));
+	},
+	updateMovie: function (req, res) {
+		console.log(`updating ${req.body.type} of ${req.body.movie} to ${req.body.value} on ${req.params.user}'s list`);
+		if (req.body.type === "status") {
+			console.log("in status");
+			db.User
+				.updateOne(
+					{ username: req.params.user, "movieArr.title": req.body.movie },
+					{ $set: {"movieArr.$.status": req.body.value} }
+				).then(dbModel => {
+					console.log(dbModel);
+					res.json(dbModel)
+				})
+				.catch(err => res.status(422).json(err));
+		} else if (req.body.type === "score") {
+			console.log("in score");
+			db.User
+				.updateOne(
+					{ username: req.params.user, "movieArr.title": req.body.movie },
+					{ $set: {"movieArr.$.score": req.body.value} }
+				).then(dbModel => {
+					console.log(dbModel);
+					res.json(dbModel)
+				})
+				.catch(err => res.status(422).json(err));
+		}
 	}
 };
