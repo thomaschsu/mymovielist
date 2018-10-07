bbd7e109b396e1b49b245d157b69302f78c00db9
Tom Hsu <thomaschsu@gmail.com>
2018-10-07 15:49:58 -0400

    Added a little css and modified modal

--- client/src/components/SideNav/SideNav.js
+++ client/src/components/SideNav/SideNav.js
@@ -19,33 +19,32 @@ class SideNav extends Component {
 
 
   render() {
-    return(
-    <div className="SideNav animated fadeInLeftBig delay-1s">
-      <ul>
+    return (
+      <div className="SideNav animated fadeInLeftBig delay-1s">
+        <ul>
 
-        <li>
-          <a className="small material-icons modal-trigger" href="#modal2" id="add">playlist_add</a>
-          <div id="modal2" className="modal modal-fixed-footer">
-            <div className="modal-content">
-              <h4>Modal Header</h4>
-              <p>A bunch of text</p>
+          <li>
+            <a className="small material-icons modal-trigger" href="#modal2" id="add">playlist_add</a>
+            <div id="modal2" className="modal modal-fixed-footer">
+              <div className="modal-content">
+                <h4>Quick Add Movie</h4>
+                <div class="md-form">
+                  <input class="form-control" type="text" placeholder="Please Enter a Movie Title" aria-label="Search"></input>
+                </div>
+              </div>
             </div>
-            <div className="modal-footer">
-              <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
-            </div>
-          </div>
-        </li>
+          </li>
 
-        <li>
-          <a className="small material-icons" href="##" id="chat" >chat</a>
-        </li>
+          <li>
+            <a className="small material-icons" href="##" id="chat" >chat</a>
+          </li>
 
-        <li>
-          <a className="small material-icons" href="##" id="exit" onClick={this.handleUserLogout}>exit_to_app </a>
-        </li>
+          <li>
+            <a className="small material-icons" href="##" id="exit" onClick={this.handleUserLogout}>exit_to_app </a>
+          </li>
 
-      </ul>
-    </div>
+        </ul>
+      </div>
     )
   }
 }
