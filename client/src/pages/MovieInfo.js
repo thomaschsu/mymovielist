73d8fe44b1ce46a47bdc0c5b95d94c6f6408ffac
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-07 14:15:41 -0400

    fixed crash on incorrect IMDB ID

--- client/src/pages/MovieInfo.js
+++ client/src/pages/MovieInfo.js
@@ -16,32 +16,36 @@ class MovieInfo extends Component {
 		API.searchOne(movieId).then(res => {
 			this.setState({
 				movie: res.data
-			}, () => {console.log(this.state.movie.Ratings[0].Value)});
+			}, () => {console.log(this.state.movie)});
 		});
 	}
 
 	render() {
 		return(
 			<Wrapper>
-				<img src={this.state.movie.Poster} alt="Movie Poster"></img>
-				<h2>{this.state.movie.Title}</h2>
-				<span>{this.state.movie.Year}</span>
-				<p>{this.state.movie.Plot}</p>
-				<p>Genres: {this.state.movie.Genre}</p>
-				<p>Rated: {this.state.movie.Rated}</p>
-				<p>Director: {this.state.movie.Director}</p>
-				<p>Actors: {this.state.movie.Actors}</p>
-				<p>Writer: {this.state.movie.Writer}</p>
-				<p>IMDB Score: {this.state.movie.Ratings[0].Value}</p>
-				<p>Rotten Tomatoes Score: {this.state.movie.Ratings[1].Value}</p>
-				<p>Metacritic Score: {this.state.movie.Ratings[2].Value}</p>
-				<p>Country of Origin: {this.state.movie.Country}</p>
-				<p>Original Language: {this.state.movie.Language}</p>
-				<p>US Release: {this.state.movie.Released}</p>
-				<p>Runtime: {this.state.movie.Runtime}</p>
-				<p>Box Office Revenue: {this.state.movie.BoxOffice}</p>
-				<p>Producer: {this.state.movie.Production}</p>
-				<p>Awards: {this.state.movie.Awards}</p>
+				<img src={this.state.movie.Poster === "N/A" || !this.state.movie.Poster ? "/nopicture.png" : this.state.movie.Poster} alt="Movie Poster"></img>
+				{this.state.movie.Response === "True" ? (
+					<div>
+						<h2>{this.state.movie.Title}</h2>
+						<span>{this.state.movie.Year}</span>
+						<p>{this.state.movie.Plot}</p>
+						<p>Genres: {this.state.movie.Genre}</p>
+						<p>Rated: {this.state.movie.Rated}</p>
+						<p>Director: {this.state.movie.Director}</p>
+						<p>Actors: {this.state.movie.Actors}</p>
+						<p>Writer: {this.state.movie.Writer}</p>
+						<p>IMDB Score: {this.state.movie.Ratings[0].Value}</p>
+						<p>Rotten Tomatoes Score: {this.state.movie.Ratings[1].Value}</p>
+						<p>Metacritic Score: {this.state.movie.Ratings[2].Value}</p>
+						<p>Country of Origin: {this.state.movie.Country}</p>
+						<p>Original Language: {this.state.movie.Language}</p>
+						<p>US Release: {this.state.movie.Released}</p>
+						<p>Runtime: {this.state.movie.Runtime}</p>
+						<p>Box Office Revenue: {this.state.movie.BoxOffice}</p>
+						<p>Producer: {this.state.movie.Production}</p>
+						<p>Awards: {this.state.movie.Awards}</p>
+					</div>
+				) : <h2>Movie not found</h2>}
 			</Wrapper>
 		);
 	}
