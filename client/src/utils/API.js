3213e9eddb8d042bf33903fc461d4c2f09011dda
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-07 13:21:27 -0400

    changed /api/movies routes to /api/list

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,15 +8,15 @@ export default {
 		return axios.get(`http://www.omdbapi.com/?apikey=d546d0cf&i=${id}`);
 	},
 	getMovies: function(user) {
-		return axios.get(`/api/movies/${user}`);
+		return axios.get(`/api/list/${user}`);
 	},
 	addMovie: function(user, movie) {
-		return axios.post(`/api/movies/${user}`, movie);
+		return axios.post(`/api/list/${user}`, movie);
 	},
 	updateMovie: function(user, movie, type, value) {
-		return axios.put(`/api/movies/${user}`, {movie: movie, type: type, value: value});
+		return axios.put(`/api/list/${user}`, {movie: movie, type: type, value: value});
 	},
 	removeMovie: function(user, movie) {
-		return axios.put(`/api/movies/${user}`, {movie: movie, type: "remove"});
+		return axios.put(`/api/list/${user}`, {movie: movie, type: "remove"});
 	}
 }
