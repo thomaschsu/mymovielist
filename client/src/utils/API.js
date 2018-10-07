aef9572f66395df04979cb525f84437338176b9c
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-07 14:51:55 -0400

    restricted search to only movies and added page parameter

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default {
-	search: title => {
-		return axios.get(`http://www.omdbapi.com/?apikey=d546d0cf&s=${title}`);
+	search: (title, page) => {
+		return axios.get(`http://www.omdbapi.com/?apikey=d546d0cf&s=${title}&type=movie&page=${page}`);
 	},
 	searchOne: id => {
 		return axios.get(`http://www.omdbapi.com/?apikey=d546d0cf&i=${id}`);
