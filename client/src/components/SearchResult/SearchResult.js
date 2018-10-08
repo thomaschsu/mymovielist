9b496357e05d94411b3732a3956c2219488dc775
Tom Hsu <thomaschsu@gmail.com>
2018-10-07 21:20:36 -0400

    Added video button for searchlist

--- client/src/components/SearchResult/SearchResult.js
+++ client/src/components/SearchResult/SearchResult.js
@@ -4,8 +4,8 @@ import "./SearchResult.css"
 const SearchResult = props => (
 	<div className="movSearch animated fadeInDownBig delay-2s">
 		<img src={props.image === "N/A" || !props.image ? "/nopicture.png" : props.image} alt="movie poster, description, and release date"></img>
-		<a onClick={props.click} className="btn-floating btn-med waves-effect waves-light red"><i className="material-icons" data-imdb={props.imdb}>add</i></a>
-		<br />
+		<a onClick={props.click} className="btn-floating btn-med waves-effect waves-light red"><i className="material-icons">ondemand_video</i></a>
+		<a onClick={props.click} className="btn-floating btn-med waves-effect waves-light green"><i className="material-icons" data-imdb={props.imdb}>add</i></a>
 		<div className="titleContainer">
 		<span className="movTitle">{props.title} </span>
 		<span className="movYear">({props.year})</span>
