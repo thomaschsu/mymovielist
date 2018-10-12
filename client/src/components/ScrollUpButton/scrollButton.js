5dc902f549cfc610915b8ad342797458028cab22
Tom Hsu <thomaschsu@gmail.com>
2018-10-11 23:28:05 -0400

    Removed function from scroll button

--- client/src/components/ScrollUpButton/scrollButton.js
+++ client/src/components/ScrollUpButton/scrollButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import './scrollButton.css';
 
 const ScrollButton = () => (
-    <button onClick="topFunction()" id="myBtn" title="Go to top"><i className="med material-icons">arrow_upward</i>
+    <button id="myBtn" title="Go to top"><i className="med material-icons">arrow_upward</i>
 </button>
 );
 
