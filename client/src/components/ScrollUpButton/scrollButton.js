43537d16b79692a2f50d1d862dc320e2952f3b70
Tom Hsu <thomaschsu@gmail.com>
2018-10-11 23:26:35 -0400

    Fixed some more of the scroll button

--- client/src/components/ScrollUpButton/scrollButton.js
+++ client/src/components/ScrollUpButton/scrollButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import './scrollButton.css';
 
 const ScrollButton = () => (
-    <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="med material-icons">arrow_upward</i>
+    <button onClick="topFunction()" id="myBtn" title="Go to top"><i className="med material-icons">arrow_upward</i>
 </button>
 );
 
