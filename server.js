11a2f51886f184dbfcc6574599b180b49988d39f
Tom Hsu <thomaschsu@gmail.com>
2018-10-11 23:21:55 -0400

    Fixed newurlparser error

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ app.use(express.static("client/build"));
 app.use(routes);
 //app.use('/auth', require('./auth'));
 
+// If deployed, use the deployed database. Otherwise use the local newsscrape database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mymovielist";
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mymovielist");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Start the API server
 app.listen(PORT, function() {
