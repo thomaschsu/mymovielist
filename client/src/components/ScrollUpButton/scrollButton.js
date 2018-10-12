a75583bdf45e5d3c72e77b7ae82054db840015f2
Tom Hsu <thomaschsu@gmail.com>
2018-10-11 23:14:31 -0400

    Modified Dan's scroll to top button

--- client/src/components/ScrollUpButton/scrollButton.js
+++ client/src/components/ScrollUpButton/scrollButton.js
@@ -1,13 +1,9 @@
 import React from "react";
 import './scrollButton.css';
 
-
-
 const ScrollButton = () => (
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-    
+    <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="med material-icons">arrow_upward</i>
+</button>
 );
-
-
 
 export default ScrollButton;
