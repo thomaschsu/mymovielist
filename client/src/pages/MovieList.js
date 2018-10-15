5d134431f84366a39ba5ff7f05a33e65110a936d
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-15 16:12:37 -0400

    sort movie list by clicking on table headers

--- client/src/pages/MovieList.js
+++ client/src/pages/MovieList.js
@@ -12,12 +12,13 @@ class MovieList extends Component {
 		movies: [],
 		allMovies: [],
 		username: "",
-		status: "all"
+		status: "all",
+		sort: ""
 	};
 
 	getMovies = cb => {
 		API.getMovies(this.state.username)
-			.then(res => this.setState({ allMovies: res.data[0].movieArr }, cb))
+		.then(res => this.setState({ allMovies: res.data[0].movieArr.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)) }, cb))
 			.catch(err => console.log(err));
 	}
 
@@ -26,6 +27,41 @@ class MovieList extends Component {
 			movies: this.state.allMovies
 		}, cb);
 	};
+
+	handleSort = event => {
+		console.log("handle sort");
+		let sorted = this.state.movies;
+ 		this.setState({
+			sort: event.target.textContent
+		}, () => {
+			switch (this.state.sort) {
+				case "Score":
+					sorted.sort((a, b) => b.score - a.score);
+					this.setState({
+						movies: sorted
+					});
+					break;
+ 				case "Status":
+					sorted.sort((a,b) => (a.status > b.status) ? 1 : ((b.status > a.status) ? -1 : 0));
+					this.setState({
+						movies: sorted
+					});
+					break;
+ 				case "Director":
+					sorted.sort((a,b) => (a.director > b.director) ? 1 : ((b.director > a.director) ? -1 : 0));
+					this.setState({
+						movies: sorted
+					});
+					break;
+ 				default:
+					sorted.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+					this.setState({
+						movies: sorted
+					});
+					break;
+			}
+		});
+ 	}
 
 	handleStatusChange = event => {
 		switch (event.target.textContent) {
@@ -149,7 +185,7 @@ class MovieList extends Component {
 						<h1 className="jumbo-small">HOW MANY MOVIES HAVE YOU SEEN?</h1>
 					</Jumbotron>
 					<MovieNav currentUser={this.state.username === sessionStorage.getItem("username").slice(1, -1)} status={this.state.status} username={this.state.username} function={this.handleStatusChange}></MovieNav>
-					<List currentUser={this.state.username === sessionStorage.getItem("username").slice(1, -1)} delete={this.handleRemove} dropdown={this.handleDropdowns} movies={this.state.movies}/>
+					<List currentUser={this.state.username === sessionStorage.getItem("username").slice(1, -1)} sort={this.handleSort} delete={this.handleRemove} dropdown={this.handleDropdowns} movies={this.state.movies}/>
 				</Wrapper>
 				<SideNav />
 			</div>
